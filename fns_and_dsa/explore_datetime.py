5a766efbd89806e6c61615e51f40e5dfbb46ca0c
Shimaa_abulfadl <shimaaabuelfadl@gmail.com>
2025-06-19 19:13:46 +0300

    add file

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+import datetime
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    return current_date
+
+print(f"Current date and time: {display_current_datetime()}")
+number_of_days = int(input("Enter the number of days to add to the current date: "))
+
+def calculate_future_date():
+    future_date = datetime.timedelta(days=number_of_days) + display_current_datetime()
+    return future_date
+
+print(f"Future date: {calculate_future_date()}")
+    
