7f7aff2fb8beee839019ef946f3f715d0cd0b1be
Shimaa_abulfadl <shimaaabuelfadl@gmail.com>
2025-06-19 16:56:53 +0300

    update file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1,num2,operation):
-    if operation == '+':
+    if operation == "add":
         return num1+num2
-    elif operation == '-':
+    elif operation == "subtract":
         return num1-num2
-    elif operation == '*':
+    elif operation == "multiply":
         return num1*num2
-    elif operation == '/':
+    elif operation == "divide":
         if num2 == 0:
             return f"can't recognize this value."
         else:
